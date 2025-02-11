8f7533825ca6611ddfc6a947e36830f45fb0a7e9
PrinceSoni4090 <prince4090@gmail.com>
2025-02-11 12:14:33 +0530

    wip5

--- components/header.jsx
+++ components/header.jsx
@@ -9,8 +9,11 @@ import { Stars } from 'lucide-react';
 import { ChevronDown } from 'lucide-react';
 import { FileText } from 'lucide-react';
 import { PenBox } from 'lucide-react';
+import { checkUser } from '@/lib/checkUser';
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
+  console.log("checkuser:", checkUser);
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
