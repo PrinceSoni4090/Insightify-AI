9911d3b61379e0702546b79a92f2e77c8116b384
PrinceSoni4090 <prince4090@gmail.com>
2025-02-24 20:32:16 +0530

    wip10

--- app/(main)/interview/_components/quiz.jsx
+++ app/(main)/interview/_components/quiz.jsx
@@ -3,8 +3,11 @@
 import { generateQuiz } from "@/actions/interview";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import useFetch from "@/hooks/use-fetch";
-import { useState } from "react";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import { useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
 
 const Quiz = () => {
 
@@ -14,9 +17,19 @@ const Quiz = () => {
 
     const {
         loading: generatingQuiz,
-        fn: generatingQuizFn,
+        fn: generateQuizFn,
         data: quizData,
     } = useFetch(generateQuiz);
+
+    useEffect(() => {
+        if (quizData) {
+            setAnswers(new Array(quizData.length).fill(null));
+        }
+    }, [quizData]);
+
+    if (generatingQuiz) {
+        return <BarLoader className="mt-4" width={"100%"} color="gray" />;
+    }
 
     if (!quizData) {
         return (
@@ -31,16 +44,42 @@ const Quiz = () => {
                     </p>
                 </CardContent>
                 <CardFooter>
-                    <Button className="w-full">Start Quiz</Button>
+                    <Button className="w-full" onClick={generateQuizFn}>Start Quiz</Button>
                 </CardFooter>
             </Card>
         );
     }
 
+    const question = quizData[currentQuestion];
+
     return (
-        <div>
-            quiz
-        </div>
+        <Card className="mx-2">
+            <CardHeader>
+                <CardTitle>
+                    Question {currentQuestion + 1} of {quizData.length}
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <p className="text-lg font-medium">
+                    {question.question}
+                </p>
+
+                <RadioGroup className="space-y-2">
+                    {question.options.map((option, index) => {
+                        return (
+                            <div className="flex items-center space-x-2" key={index}>
+                                <RadioGroupItem value={option} id={`option-${index}`} />
+                                <Label htmlFor={`option-${index}`}>{option}</Label>
+                            </div>
+                        );
+                    })}
+                </RadioGroup>
+
+            </CardContent>
+            <CardFooter>
+
+            </CardFooter>
+        </Card>
     );
 };
 
