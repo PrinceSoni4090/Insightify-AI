f3c81fa877311e3733d77b38cbc890e717cbf282
PrinceSoni4090 <prince4090@gmail.com>
2025-03-03 19:54:44 +0530

    wip-13

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -10,7 +10,7 @@ const InterviewPage = async () => {
         <div>
             <div>
                 <h1 className="text-6xl font-bold gradient-title mb-5"> Interview Prep</h1>
-                <div>
+                <div className="space-y-6">
                     <StatsCards assessments={assessments} />
                     <PerformanceChart assessments={assessments} />
                     <QuizList assessments={assessments} />
