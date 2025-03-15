4f00cdc3be0aef2eb68533fc21978170ec7ab35c
PrinceSoni4090 <prince4090@gmail.com>
2025-03-15 17:38:18 +0530

    wip16

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ import {
 } from '@/components/ui/accordion';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
+import { HoverEffect } from '@/components/ui/card-hover-effect';
 import { faqs } from '@/data/faqs';
 import { features } from '@/data/features';
 import { howItWorks } from '@/data/howItWorks';
@@ -24,7 +25,9 @@ export default function Home() {
           <h2 className=" text-3xl font-bold tracking-tighter text-center mb-12">
             Powerful features for your Career Growth
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 gap-6 max-w-6xl mx-auto">
+          <div
+            className="grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 gap-6 max-w-6xl mx-auto"
+          >
             {features.map((feature, index) => {
               return (
                 <Card
@@ -78,7 +81,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
             {howItWorks.map((item, index) => (
               <div key={index} className="flex flex-col items-center text-center space-y-4">
                 <div className="w-16 h-16 rounded-full bg-primary/10 flex items-center justify-center">
