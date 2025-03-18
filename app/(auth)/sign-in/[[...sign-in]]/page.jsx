e6a0f8ab03cc260a7b932096d1409d02d576119f
PrinceSoni4090 <prince4090@gmail.com>
2025-03-18 20:02:47 +0530

    crash fixes

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,7 +1,20 @@
 import { SignIn } from '@clerk/nextjs';
 
 const Page = () => {
-    return <SignIn />;
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-semibold text-black dark:text-white mb-5">Demo Credentials</h1>
+        <h2 className="text-lg font-base text-black dark:text-white mb-5">
+          Username - Priceop4090@gmail.com{' '}
+        </h2>
+        <h2 className="text-lg font-base text-black dark:text-white mb-5">
+          Password - Prince4090@
+        </h2>
+        <SignIn />
+      </div>
+    </>
+  );
 };
 
 export default Page;
